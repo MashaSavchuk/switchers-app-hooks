94ba89f9991b5f28c25b505a989a9ac45a3a627f
Maria Savchuk <savchuk_maria@ukr.net>
2023-01-14 17:36:58 +0000

    Created switcher react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+// function App() {
+const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <h1>Counter</h1>
+        <h2>8</h2>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
